d5125e5a2b16ee1e3c9563b4f2a54ea6bb2ac02c
Hoàng Minh Thắng <p@banphim.net>
2017-08-31 17:34:43 +0700

    use cljc to support clojurescript

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject n2w-vi "0.1.0"
+(defproject n2w-vi "0.2.0"
   :description "number to Vietnamese word conversion"
   :url "https://github.com/myguidingstar/number-to-words_vi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
